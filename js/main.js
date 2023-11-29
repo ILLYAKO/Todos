c2f05a351e308453f8b5d55c013f072720a4b1f4
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2023-11-29 15:09:17 -0500

    changed main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// import addTask from "./addTask.js";
+
+export default addTask = () => {
+    let input = document.querySelector("input#addTaskInput");
+    let taskText = input.value;
+    if (taskText) {
+        tasks.unshift(taskText);
+        input.value = null;
+    } else {
+        input.placeholder = "PLEASE ADD TASK!!!";
+    }
+};
