6a06c1624651a3420ea7473f0db36df31b9e45ed
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2023-11-29 17:05:52 -0500

    short addTask

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-import { addTask } from "./addTask.js";
-export default { addTask };
+let tasks = [];
+let taskInput = document.getElementById("inputTask");
+console.log("taskInput: " + taskInput);
+let ulTasks = document.getElementById("taskList");
+console.log("Hello from main.js");
+const addTask = () => {
+    tasks.push(taskInput.value);
+    taskInput.value = "";
+    let stringLi = "";
+    tasks.forEach((task) => {
+        stringLi += `<li>${task}</li>`;
+    });
+    ulTasks.innerHTML = stringLi;
+};
