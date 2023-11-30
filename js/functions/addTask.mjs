1f81666c99c2e50aa929047b25eba92d05fc3fc6
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2023-11-30 12:26:41 -0500

    added localstorage

--- js/functions/addTask.mjs
+++
@@ -1,9 +0,0 @@
-const addTask = () => {
-    tasks.push(taskInput.value);
-    taskInput.value = "";
-    let stringLi = "";
-    tasks.forEach((task) => {
-        stringLi += `<li>${task}</li>`;
-    });
-    ulTasks.innerHTML = stringLi;
-};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
-let tasks = [];
+let localTasks = localStorage.getItem("myTasks");
+let tasks = localTasks ? localTasks.split(",") : [];
+let formElement = document.getElementById("addTaskForm");
 let taskInput = document.getElementById("inputTask");
-let ulTasks = document.getElementById("taskList");
+
+const addTask = () => {
+    tasks.push(taskInput.value);
+    taskInput.value = "";
+    localStorage.setItem("myTasks", tasks);
+    renderList();
+};
+
+const renderList = () => {
+    if (!tasks) {
+        return;
+    }
+
+    if (document.getElementById("ulTasks")) {
+        document.getElementById("ulTasks").outerHTML = "";
+    }
+    const ulElement = document.createElement("ul");
+    ulElement.setAttribute("id", "ulTasks");
+    formElement.insertAdjacentElement("afterend", ulElement);
+
+    tasks.forEach((task) => {
+        let liElement = document.createElement("li");
+        liElement.appendChild(document.createTextNode(task));
+        ulElement.appendChild(liElement);
+    });
+};
+
+renderList();
