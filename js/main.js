86375706792a6263d1acf154aae3de056447db29
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2023-11-30 17:02:29 -0500

    added buttons with createElement

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
+debugger;
 let localTasks = localStorage.getItem("myTasks");
 let tasks = localTasks ? localTasks.split(",") : [];
 let formElement = document.getElementById("addTaskForm");
 let taskInput = document.getElementById("inputTask");
+
+const editTask = (id) => {
+    console.log("Edit");
+};
+
+const deleteTask = (id) => {
+    console.log("Delete");
+};
 
 const addTask = () => {
     tasks.push(taskInput.value);
@@ -11,20 +20,48 @@ const addTask = () => {
 };
 
 const renderList = () => {
-    if (!tasks) {
+    if (tasks.length == 0) {
         return;
     }
 
     if (document.getElementById("ulTasks")) {
         document.getElementById("ulTasks").outerHTML = "";
     }
+
     const ulElement = document.createElement("ul");
     ulElement.setAttribute("id", "ulTasks");
     formElement.insertAdjacentElement("afterend", ulElement);
 
     tasks.forEach((task) => {
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.name = "checkboxName";
+        checkbox.value = "checkboxValue";
+        checkbox.id = "checkboxId";
+
+        let editBtn = document.createElement("button");
+        editBtn.type = "button";
+        editBtn.name = "editBtnName";
+        editBtn.value = "editBtnValue";
+        editBtn.id = "editBtnId";
+        editBtn.onclick = () => editTask();
+        editBtn.appendChild(document.createTextNode("Edit"));
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.type = "button";
+        deleteBtn.name = "deleteBtnName";
+        deleteBtn.value = "deleteBtnValue";
+        deleteBtn.id = "deleteBtnId";
+        deleteBtn.onclick = () => deleteTask();
+        deleteBtn.appendChild(document.createTextNode("Delete"));
+
         let liElement = document.createElement("li");
+        liElement.id = "liElementId";
+        liElement.appendChild(checkbox);
         liElement.appendChild(document.createTextNode(task));
+        liElement.appendChild(editBtn);
+        liElement.appendChild(deleteBtn);
+
         ulElement.appendChild(liElement);
     });
 };
